7098b3fffe6a96a56cd66692f1955cdd73cfa93e
cparros <celparros@gmail.com>
2021-05-19 10:10:10 -0400

    header and footers created with router links to prevent refreshes

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,14 @@
     </div>
     
     <Tasks @toggle-reminder="toggleReminder" @delete-task='deleteTask' :tasks="tasks"/>
+    <router-view></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
@@ -18,6 +21,7 @@ export default {
   name: 'App',
   components: {
     Header,
+    Footer,
     Tasks,
     AddTask
   },
